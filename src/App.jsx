2faf21df9eff8597c8b296b9beb25975a0e14757
N Rizki Amalia <rizkiamel9@gmail.com>
2025-06-22 05:41:46 +0700

    feat: create cocktails, art, menu, and footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,7 +15,11 @@ const App = () => {
     <main>
         <Navbar />
         <Hero />
-        <div className='h-dvh bg-black' />
+        <Cocktails />
+        <About />
+        <Art />
+        <Menu />
+        <Contact />
     </main>
   )
 }
